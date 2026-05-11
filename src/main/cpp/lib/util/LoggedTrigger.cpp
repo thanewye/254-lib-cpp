948371ffe26ab1e08b2aeb26db1675f1b6c93e25
Ethan Ye <ethan.w.ye@gmail.com>
2026-05-10 17:38:59 -0700

    logged trigger

--- src/main/cpp/lib/util/LoggedTrigger.cpp
+++ src/main/cpp/lib/util/LoggedTrigger.cpp
@@ -1,3 +1,87 @@
 #include "lib/util/LoggedTrigger.h"
 
-// TODO: implement
+#include <string>
+#include <vector>
+
+#include <frc2/command/Command.h>
+#include <frc2/command/InstantCommand.h>
+
+#include "akit/Logger.h"
+#include "lib/commands/ChezySequenceCommandGroup.h"
+#include "lib/time/RobotTime.h"
+
+LoggedTrigger::LoggedTrigger() : frc2::Trigger() {}
+
+LoggedTrigger::LoggedTrigger(std::function<bool()> condition)
+    : frc2::Trigger(std::move(condition)) {}
+
+LoggedTrigger::LoggedTrigger(frc::EventLoop* loop, std::function<bool()> condition)
+    : frc2::Trigger(loop, std::move(condition)) {}
+
+LoggedTrigger LoggedTrigger::Wrap(std::function<bool()> condition) {
+    return LoggedTrigger(std::move(condition));
+}
+
+LoggedTrigger LoggedTrigger::Wrap(frc2::Trigger trigger) {
+    return LoggedTrigger([trigger] { return trigger.Get(); });
+}
+
+frc2::CommandPtr LoggedTrigger::WrapCommand(frc2::CommandPtr&& command, const std::source_location& loc) {
+    std::string cmdName = command.get()->GetName();
+    frc2::Command::InterruptionBehavior interruptBehavior = command.get()->GetInterruptionBehavior();
+    std::string key = std::string("LoggedTriggers/") + loc.file_name() + "_" +
+                      std::to_string(loc.line()) + "/" + cmdName;
+
+    frc2::CommandPtr logCmd = frc2::InstantCommand([key] {
+        akit::Logger::RecordOutput(key, RobotTime::GetTimestampSeconds());
+    }).IgnoringDisable(true);
+
+    std::vector<frc2::CommandPtr> cmds;
+    cmds.push_back(std::move(logCmd));
+    cmds.push_back(std::move(command));
+
+    return frc2::CommandPtr(std::make_unique<ChezySequenceCommandGroup>(std::move(cmds)))
+        .WithName(cmdName)
+        .WithInterruptBehavior(interruptBehavior);
+}
+
+frc2::Trigger LoggedTrigger::OnTrue(frc2::CommandPtr&& command, std::source_location loc) {
+    return frc2::Trigger::OnTrue(WrapCommand(std::move(command), loc));
+}
+
+frc2::Trigger LoggedTrigger::OnFalse(frc2::CommandPtr&& command, std::source_location loc) {
+    return frc2::Trigger::OnFalse(WrapCommand(std::move(command), loc));
+}
+
+frc2::Trigger LoggedTrigger::WhileTrue(frc2::CommandPtr&& command, std::source_location loc) {
+    return frc2::Trigger::WhileTrue(WrapCommand(std::move(command), loc));
+}
+
+frc2::Trigger LoggedTrigger::WhileFalse(frc2::CommandPtr&& command, std::source_location loc) {
+    return frc2::Trigger::WhileFalse(WrapCommand(std::move(command), loc));
+}
+
+frc2::Trigger LoggedTrigger::ToggleOnTrue(frc2::CommandPtr&& command, std::source_location loc) {
+    return frc2::Trigger::ToggleOnTrue(WrapCommand(std::move(command), loc));
+}
+
+frc2::Trigger LoggedTrigger::ToggleOnFalse(frc2::CommandPtr&& command, std::source_location loc) {
+    return frc2::Trigger::ToggleOnFalse(WrapCommand(std::move(command), loc));
+}
+
+LoggedTrigger LoggedTrigger::operator&&(frc2::Trigger rhs) const {
+    return LoggedTrigger([self = *this, rhs] { return self.Get() && rhs.Get(); });
+}
+
+LoggedTrigger LoggedTrigger::operator||(frc2::Trigger rhs) const {
+    return LoggedTrigger([self = *this, rhs] { return self.Get() || rhs.Get(); });
+}
+
+LoggedTrigger LoggedTrigger::operator!() const {
+    return LoggedTrigger([self = *this] { return !self.Get(); });
+}
+
+LoggedTrigger LoggedTrigger::Debounce(units::second_t debounceTime, frc::Debouncer::DebounceType type) {
+    frc2::Trigger wrapped = frc2::Trigger::Debounce(debounceTime, type);
+    return LoggedTrigger([wrapped] { return wrapped.Get(); });
+}
--- src/main/cpp/lib/util/LoggedTrigger.h
+++ src/main/cpp/lib/util/LoggedTrigger.h
@@ -1,20 +1,43 @@
 #pragma once
 
 #include <functional>
-#include <string>
+#include <source_location>
 
+#include <frc/event/EventLoop.h>
+#include <frc/filter/Debouncer.h>
+#include <frc2/command/CommandPtr.h>
 #include <frc2/command/button/Trigger.h>
+#include <units/time.h>
 
 class LoggedTrigger : public frc2::Trigger {
- public:
-  LoggedTrigger();
-  explicit LoggedTrigger(std::function<bool()> condition);
+public:
+    LoggedTrigger();
+    explicit LoggedTrigger(std::function<bool()> condition);
+    LoggedTrigger(frc::EventLoop* loop, std::function<bool()> condition);
 
-  Trigger OnTrue(frc2::CommandPtr command) &&;
-  Trigger OnFalse(frc2::CommandPtr command) &&;
-  Trigger WhileTrue(frc2::CommandPtr command) &&;
-  Trigger WhileFalse(frc2::CommandPtr command) &&;
-  LoggedTrigger operator&&(Trigger other) const;
-  LoggedTrigger operator||(Trigger other) const;
-  LoggedTrigger operator!() const;
+    static LoggedTrigger Wrap(std::function<bool()> condition);
+    static LoggedTrigger Wrap(frc2::Trigger trigger);
+
+    frc2::Trigger OnTrue(frc2::CommandPtr&& command,
+                         std::source_location loc = std::source_location::current());
+    frc2::Trigger OnFalse(frc2::CommandPtr&& command,
+                          std::source_location loc = std::source_location::current());
+    frc2::Trigger WhileTrue(frc2::CommandPtr&& command,
+                            std::source_location loc = std::source_location::current());
+    frc2::Trigger WhileFalse(frc2::CommandPtr&& command,
+                             std::source_location loc = std::source_location::current());
+    frc2::Trigger ToggleOnTrue(frc2::CommandPtr&& command,
+                               std::source_location loc = std::source_location::current());
+    frc2::Trigger ToggleOnFalse(frc2::CommandPtr&& command,
+                                std::source_location loc = std::source_location::current());
+
+    LoggedTrigger operator&&(frc2::Trigger rhs) const;
+    LoggedTrigger operator||(frc2::Trigger rhs) const;
+    LoggedTrigger operator!() const;
+
+    LoggedTrigger Debounce(units::second_t debounceTime,
+                           frc::Debouncer::DebounceType type = frc::Debouncer::DebounceType::kRising);
+
+private:
+    frc2::CommandPtr WrapCommand(frc2::CommandPtr&& command, const std::source_location& loc);
 };
