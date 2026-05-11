b909a85e4efbe0d9c990d46baf00e57486fe3961
Ethan Ye <ethan.w.ye@gmail.com>
2026-05-10 17:39:10 -0700

    implement state machine

--- src/main/cpp/lib/util/StateMachine.cpp
+++ src/main/cpp/lib/util/StateMachine.cpp
@@ -1,3 +1,91 @@
 #include "lib/util/StateMachine.h"
 
-// TODO: implement
+#include <cstdio>
+
+#include "akit/Logger.h"
+#include "lib/time/RobotTime.h"
+
+StateMachine::State::State(std::string name, StateMachine* machine)
+    : name(std::move(name)), machine(machine) {}
+
+void StateMachine::State::AddTransition(StateCondition condition, std::string targetState) {
+    transitions.push_back({std::move(condition), std::move(targetState)});
+}
+
+LoggedTrigger StateMachine::State::RegisterTrigger(StateCondition condition) {
+    return LoggedTrigger([this, condition = std::move(condition)] {
+        if (!machine->currentState.has_value() || machine->currentState.value() != name) {
+            return false;
+        }
+        double timeInState = (RobotTime::GetTimestamp() - machine->lastStageChangeTime).value();
+        return condition(timeInState);
+    });
+}
+
+LoggedTrigger StateMachine::State::RegisterTrigger(std::function<bool()> condition) {
+    return RegisterTrigger([condition = std::move(condition)](double) { return condition(); });
+}
+
+LoggedTrigger StateMachine::State::RegisterTrigger() {
+    return RegisterTrigger([](double) { return true; });
+}
+
+StateMachine::StateMachine(std::string name)
+    : name(name),
+      currentStateLogKey("StateMachines/" + name + "/CurrentState"),
+      lastStateLogKey("StateMachines/" + name + "/LastState"),
+      timeInStateLogKey("StateMachines/" + name + "/TimeInState") {}
+
+StateMachine::State& StateMachine::AddState(const std::string& name) {
+    auto it = states.find(name);
+    if (it != states.end()) {
+        return it->second;
+    }
+    auto [newIt, inserted] = states.emplace(name, State(name, this));
+    return newIt->second;
+}
+
+void StateMachine::SetInitialState(const std::string& name) {
+    if (states.contains(name)) {
+        currentState = name;
+        lastState = "";
+        lastStageChangeTime = RobotTime::GetTimestamp();
+    } else {
+        std::fprintf(stderr, "StateMachine %s: Attempted to set invalid initial state %s\n",
+                     this->name.c_str(), name.c_str());
+    }
+}
+
+std::optional<std::string> StateMachine::GetCurrentState() const {
+    return currentState;
+}
+
+void StateMachine::Update() {
+    if (!currentState.has_value()) {
+        return;
+    }
+
+    auto it = states.find(currentState.value());
+    if (it != states.end()) {
+        double timeInState = (RobotTime::GetTimestamp() - lastStageChangeTime).value();
+        for (auto& transition : it->second.transitions) {
+            if (transition.condition(timeInState)) {
+                SetState(transition.targetState);
+                break;
+            }
+        }
+    }
+
+    double timestamp = RobotTime::GetTimestampSeconds();
+    akit::Logger::RecordOutput(currentStateLogKey, currentState.value_or(""));
+    akit::Logger::RecordOutput(lastStateLogKey, lastState);
+    akit::Logger::RecordOutput(timeInStateLogKey, timestamp - lastStageChangeTime.value());
+}
+
+void StateMachine::SetState(const std::string& newState) {
+    if (newState != currentState.value_or("") && states.contains(newState)) {
+        lastState = currentState.value_or("");
+        currentState = newState;
+        lastStageChangeTime = RobotTime::GetTimestamp();
+    }
+}
--- src/main/cpp/lib/util/StateMachine.h
+++ src/main/cpp/lib/util/StateMachine.h
@@ -1,26 +1,58 @@
 #pragma once
 
 #include <functional>
+#include <optional>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
+#include <units/time.h>
+
+#include "lib/util/LoggedTrigger.h"
+
 class StateMachine {
- public:
-  struct State {
+public:
+    using StateCondition = std::function<bool(double)>;
+
+    class State {
+    public:
+        void AddTransition(StateCondition condition, std::string targetState);
+        LoggedTrigger RegisterTrigger(StateCondition condition);
+        LoggedTrigger RegisterTrigger(std::function<bool()> condition);
+        LoggedTrigger RegisterTrigger();
+
+    private:
+        friend class StateMachine;
+
+        struct Transition {
+            StateCondition condition;
+            std::string targetState;
+        };
+
+        State(std::string name, StateMachine* machine);
+
+        std::string name;
+        StateMachine* machine;
+        std::vector<Transition> transitions;
+    };
+
+    explicit StateMachine(std::string name);
+
+    State& AddState(const std::string& name);
+    void SetInitialState(const std::string& name);
+    [[nodiscard]] std::optional<std::string> GetCurrentState() const;
+    void Update();
+
+private:
     std::string name;
-    std::function<void()> onEnter;
-    std::function<void()> onExit;
-    std::function<void()> periodic;
-  };
+    std::string currentStateLogKey;
+    std::string lastStateLogKey;
+    std::string timeInStateLogKey;
 
-  using StateCondition = std::function<bool()>;
+    std::unordered_map<std::string, State> states;
+    std::optional<std::string> currentState;
+    std::string lastState;
+    units::second_t lastStageChangeTime = 0.0_s;
 
-  void AddState(State state);
-  void SetInitialState(const std::string& name);
-  const std::string& GetCurrentState() const;
-  void Update();
-
- private:
-  std::string m_currentState;
-  std::vector<State> m_states;
+    void SetState(const std::string& newState);
 };
